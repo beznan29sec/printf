37b42a4e84a51b4ea964523626a4ae41886367d9
Bez <bezanehambachew@gmail.com>
2023-08-23 23:01:16 -0700

    Commit 0

--- _printf.c
+++ _printf.c
@@ -1,40 +1,56 @@
 #include "main.h"
 /**
  * _printf - print function
- * @format: char string
- * Returns: printed characters
+ * @format: Character string
+ * Return: Printed characters
  */
+
 
 int _printf(const char *format, ...)
 {
 	int print = 0;
-
 	va_list n;
 
-	if (format == '\0')
+	if (format == NULL)
 	{
 		return (-1);
 	}
-
 	va_start(n, format);
 
 	while (*format)
 	{
-		if (*format != '%')
+		if (format != '%')
 		{
-			write(1, format, 1);
+			write(1,  format, 1);
 			print++;
 		}
 		else
 		{
 			format++;
 		}
-		if (format == 'c' || format == 's' || format == '%')
+		if (format == '\0')
 		{
-			int x = va_arg(n, int);
+			break;
 		}
-
-			write(1, x, strlen(x));
-			va_end(n);
+		if (*format == '%')
+		{
+			write(1, format, 1);
+			print++;
+		}
+		else if (*format == 'c')
+		{
+			char c = va_arg(n, int);
+			write(1, &c, 1);
+			print++;
+		}
+		else if (*format == 's')
+		{
+			char *str = va_arg(n, char*);
+			write(1, &str, strlen(str));
+			print += str;
+		}
+		format++;
 	}
+	va_end(n);
+	return (print);
 }
